f668e24b844bbd63de30c0c39f4b8460b5b5b3eb
Kitti Pariyaakkarakul <zondezatera@gmail.com>
2020-01-13 17:04:28 +0700

    add ui auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, Form, Icon, Input, Button } from 'antd';
 
-function App() {
+
+const App = () => {
+  const title = 'Welcome ABLE EASTERN'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <section className="content">
+        <Card title={title.toUpperCase()} bordered={false} headStyle={{textAlign:'center', fontSize: 25}} style={{ width: 400 }}>
+          <Form onSubmit={() =>{}} className="login-form">
+        <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />
+        </Form.Item>
+        <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" size='large' className="login-form-button">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+        </Card>
+      </section>
     </div>
   );
 }
---
+++ src/feature/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Home extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+        home
+      </div>
+    )
+  }
+}
+
+export default Home
